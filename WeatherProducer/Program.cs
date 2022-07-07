c20ca6825b70405283d54bc89100445a0dc7cd92
MarioDubreuil <mario_dubreuil@hotmail.com>
2022-07-06 20:42:14 -0400

    format code

--- WeatherProducer/Program.cs
+++ WeatherProducer/Program.cs
@@ -1,9 +1,13 @@
 ﻿using Confluent.Kafka;
 using Newtonsoft.Json;
 
-var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+var config = new ProducerConfig
+{
+    BootstrapServers = "localhost:9092"
+};
 
 using var producer = new ProducerBuilder<Null, string>(config).Build();
+
 try
 {
     string? state;
